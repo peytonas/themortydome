5241ccde536e23de4f2daa380cff571c6e0c21d8
Peyton Sonnefeld <peyton.sonnefeld@gmail.com>
2020-01-01 14:10:34 -0700

    installed multer for image use

--- server/services/FightersService.js
+++ server/services/FightersService.js
@@ -3,11 +3,18 @@ import { stringify } from "querystring"
 const Schema = mongoose.Schema
 const ObjectId = Schema.Types.ObjectId
 
+// let image = new Schema({
+//   img: {
+//     data: Buffer, contentType: String
+//   }
+// });
+
 let _schema = new Schema({
   name: { type: String, required: true },
   number: { type: String, required: true },
   type: { type: String, enum: ["rock", "paper", "scissors", "normal"], required: true },
   imgUrl: { type: String, default: 'https://placehold.it/200x200' },
+  // image: { image },
   description: { type: String, maxlength: 500 },
   hp: { type: Number },
   hits: { type: Number, default: 0 },
